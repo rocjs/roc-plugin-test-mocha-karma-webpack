6925e8b3e3c2c19c66e28af90d8dfd46e2a5dea7
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-10-07 16:30:39 +0200

    We need the development package, not the normal package

--- extensions/roc-plugin-test-mocha-karma-webpack/src/roc/index.js
+++ extensions/roc-plugin-test-mocha-karma-webpack/src/roc/index.js
@@ -10,7 +10,7 @@ const lazyRequire = lazyFunctionRequire(require);
 
 export default {
     required: {
-        'roc-package-webpack-web': '^1.0.0-beta',
+        'roc-package-webpack-web-dev': '^1.0.0-beta',
     },
     plugins: [
         require.resolve('roc-abstract-plugin-test'),
